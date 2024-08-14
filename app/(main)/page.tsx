1d258645dc66c96b5684f3f3d802f026833fd17c
dunkeyyfong <phongtrong865@gmail.com>
2024-08-14 23:22:44 +0700

    Update font family and update Main Page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,7 @@
 import { ThemeSwitcher } from "@/components/theme-switcher";
 
 const MainPage = () => {
-  return <div>Main Page</div>;
+  return <div className="mt-[8vh]">Main Page</div>;
 };
 
 export default MainPage;
