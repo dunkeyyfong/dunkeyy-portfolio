dc1b2415441e6c0e4f1487abc2c712a0e1556516
dunkeyyfong <phongtrong865@gmail.com>
2026-01-09 11:05:26 +0700

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Box, Text } from "@chakra-ui/react";
 
 const Footer = () => {
-  return <div className="">footer</div>;
+  return (
+    <Box
+      as="footer"
+      className="mt-auto py-6 text-center"
+      borderTop="1px solid"
+      borderColor="gray.200"
+      _dark={{ borderColor: "gray.700" }}
+    >
+      <Text fontSize="sm" color="gray.600" _dark={{ color: "gray.400" }}>
+        © 2025 Nguyen Trong Phong. All rights reserved.
+      </Text>
+    </Box>
+  );
 };
 
 export default Footer;
